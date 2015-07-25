a39e8bef990ac52c3c514106c8ef89368cc43cc1
Meghdeep <meghdeepr@gmail.com>
2015-07-25 20:22:43 +0530

    Fixed shit

--- Dream/dream_site/admin.py
+++ Dream/dream_site/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Mentor, Mentee, Team, Admin
+from .models import Mentor, Mentee, Team, Admin, Meetings, Goals
 # Register your models here.
 
 admin.site.register(Mentor)
 admin.site.register(Mentee)
 admin.site.register(Team)
+admin.site.register(Meetings)
+admin.site.register(Goals)
 admin.site.register(Admin)
