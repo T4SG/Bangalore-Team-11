b06f32bc9b2dd1f004053b7acb10a15227a0a22a
Meghdeep <meghdeepr@gmail.com>
2015-07-26 02:06:54 +0530

    Massive Commit

--- Dream/dream_site/admin.py
+++ Dream/dream_site/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Mentor, Mentee, Team, Admin, Meetings, Goals
+from .models import Mentor, Mentee, Team, Admin, Meeting, Goal
 # Register your models here.
 
 admin.site.register(Mentor)
 admin.site.register(Mentee)
 admin.site.register(Team)
-admin.site.register(Meetings)
-admin.site.register(Goals)
+admin.site.register(Meeting)
+admin.site.register(Goal)
 admin.site.register(Admin)
