9ee8fed2faf340892da3b2100afb4b69156c3c3d
Meghdeep <meghdeepr@gmail.com>
2015-07-26 06:35:04 +0530

    Urls and Views

--- Dream/dream_site/urls.py
+++ Dream/dream_site/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^new_mentor/$', views.new_mentor, name = 'new_mentor')
 ]
--- Dream/dream_site/views.py
+++ Dream/dream_site/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+
 # Create your views here.
 
 def index(request):
     return HttpResponse("Dream Site Index Page")
+    
+class new_mentor( request ):
+    if request.method == "GET":
+        print("GOT GET")
+    elif request.method == "POST":
+        print('GOT POST')
+        print("request.form.items()")
+        return HttpResponse("Okay")
     
 
