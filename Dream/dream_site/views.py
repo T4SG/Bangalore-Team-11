ef910ef774561af0d4cd79a09b72c1e745e82cd1
Meghdeep <meghdeepr@gmail.com>
2015-07-26 06:45:48 +0530

    Urls and Views

--- Dream/dream_site/views.py
+++ Dream/dream_site/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
-
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def index(request):
     return HttpResponse("Dream Site Index Page")
-    
+
+@csrf_exempt
 def new_mentor( request ):
     if request.method == "GET":
         print("GOT GET")
