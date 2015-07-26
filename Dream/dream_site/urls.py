c368521361494e9e75493f6c2fab02472898db1d
Meghdeep <meghdeepr@gmail.com>
2015-07-26 09:35:13 +0530

    Debugging

--- Dream/dream_site/urls.py
+++ Dream/dream_site/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     url(r'^mentor_login/$', views.mentor_login, name = 'mentor_login'),
     url(r'^mentee_login/$', views.mentee_login, name = 'mentee_login'),
     url(r'^mentor_page/(?P<mentor_id_in>\d+)/$', views.mentor_page, name = 'mentor_page'),
-    url(r'^mentee_page/(?P<mentee_id_in>\d+)/$', views.mentee_login, name = 'mentee_page'),
+    url(r'^mentee_page/(?P<mentee_id_in>\d+)/$', views.mentee_page, name = 'mentee_page'),
 ]
