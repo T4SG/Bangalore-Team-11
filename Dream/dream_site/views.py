85212ffdbcdea737f30cf661569b8f1824edfe39
Meghdeep <meghdeepr@gmail.com>
2015-07-26 07:46:49 +0530

    View Advancement

--- Dream/dream_site/views.py
+++ Dream/dream_site/views.py
@@ -44,3 +44,41 @@ def new_mentor( request ):
         return HttpResponse("Okay")
     
 
+@csrf_exempt
+def new_mentee( request ):
+    if request.method == "GET":
+        print("GOT GET")
+        return HttpResponse("Okay")
+    elif request.method == "POST":
+#        print('GOT POST')
+#        for i in request.POST:
+#            print(i)
+        l2  = request.POST['secondary-language']
+		age = request.POST['age']
+        ad  = request.POST['address']
+        gen = request.POST['gender']
+        eml = request.POST['form-email']
+       # com = request.POST['company']
+		clg = request.POST['college']
+        ncy = request.POST['form-native-city']
+		ccy = request.POST['form-current-city']
+        l1  = request.POST['primary-language']
+		h1 = request.POST['hobby1']
+		h2 = request.POST['hobby2']
+		h3 = request.POST['hobby3']
+		h4 = request.POST['hobby4']
+		pw = hashlib.md5();
+        pw  = pw.update(request.POST['password'])
+		ar = request.POST['area']
+        n1  = request.POST['form-first-name']
+        l3  = request.POST['other-language']
+        pno = request.POST['phone-number']
+        unm = request.POST['user-name']
+        n2  = request.POST['form-last-name']
+        Mentee.objects.create( name = ( n1 + " " + n2 ), age = age, native_city  = ncy,current_city = ccy,
+								gender = gen, primary_language = l1, secondary_language = l2,other_languages = l3,
+								college_degree = clg ,hobby_1 = h1,hobby_2 = h2,hobby_3 = h3,hobby_4 = h4,
+								  college = clg, phone_number = pno , area = ar,address = ad,
+								 email = eml,username = unm, password = pw)
+        return HttpResponse("Okay")
+
